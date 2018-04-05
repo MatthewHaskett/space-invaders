881f2a8f1b249e2620ac69b895eeb605a719b867
Matthew Haskett <itztheendmc@gmail.com>
2018-04-05 10:26:38 +0100

    Create main menu

---
+++ menu.py
@@ -0,0 +1,22 @@
+import pygame
+import game
+
+
+class Menu:
+    def __init__(self):
+        pass
+
+    def open(self):
+        pass
+
+    def play(self):
+        game.start(self.window)
+
+    def high_scores(self):
+        pass
+
+
+menu = Menu()
+
+if __name__ == "__main__":
+    menu.open()
